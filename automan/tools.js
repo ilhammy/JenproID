fafb68a9248c412c50150852d660805b3095557f
Ilham Maulana Yusup <ilhamsense8@gmail.com>
2024-08-10 13:36:13 +0700

    Update tools.js

--- automan/tools.js
+++ automan/tools.js
@@ -1,16 +1,19 @@
 function autoIsi() {
-    var c = document.querySelectorAll("div.grid > button"),
-        i = 0;
-    c.forEach(function(c, n) {
-        n % 6 == 0 && (setTimeout(function() {
-            c.click()
-        }, 1e3 * i), i++)
-    })
-}
-function skipTes() {
-    document.querySelector("input[type=submit]").click()
-}
-var div = document.createElement("div");
-div.style = "position:absolute;left:50%";
-div.innerHTML = '<button class="text-white bg-[#f59e0b] hover:bg-[#f59e0b]/90 active::bg-[#f59e0b]/50 focus:outline-none focus:ring-[#f59e0b]/50 font-poppins-medium rounded-lg text-sm px-4 py-4 text-center shadow-md mx-4" onclick="skipTes()">SKIP</button><button class="text-white bg-[#f59e0b] hover:bg-[#f59e0b]/90 active::bg-[#f59e0b]/50 focus:outline-none focus:ring-[#f59e0b]/50 font-poppins-medium rounded-lg text-sm px-4 py-4 text-center shadow-md my-8" onclick="autoIsi()">AUTO</button>';
-document.querySelector('header nav div.container div.flex').appendChild(div);
+		var btns = document.querySelectorAll("div.grid > button");
+		let pos = 0;
+		btns.forEach(function(btn, n) {
+			if (n % 6 == 0) {
+				setTimeout(function() {
+					btn.click();
+				}, 1000 * pos);
+				pos++;
+			}
+		});
+	}
+	function skipTes() {
+		document.querySelector("input[type=submit]").click();
+	}
+	var div = document.createElement("div");
+	div.style = "position:absolute;left:50%";
+	div.innerHTML = '<button class="text-white bg-[#f59e0b] hover:bg-[#f59e0b]/90 active::bg-[#f59e0b]/50 focus:outline-none focus:ring-[#f59e0b]/50 font-poppins-medium rounded-lg text-sm px-4 py-4 text-center shadow-md mx-4" onclick="skipTes()">SKIP</button><button class="text-white bg-[#f59e0b] hover:bg-[#f59e0b]/90 active::bg-[#f59e0b]/50 focus:outline-none focus:ring-[#f59e0b]/50 font-poppins-medium rounded-lg text-sm px-4 py-4 text-center shadow-md my-8" onclick="autoIsi()">AUTO</button>';
+	document.querySelector('header nav div.container div.flex').appendChild(div);
